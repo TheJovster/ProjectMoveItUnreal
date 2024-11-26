3382174188662d9af1ea72734690e52fe181a61b
JovanAleksicFutureGames <145547495+JovanAleksicFutureGames@users.noreply.github.com>
2024-11-26 18:28:22 +0100

    Basic Character movement implemented

--- Source/ProjectMoveItUnreal/Private/Characters/Player/PlayerCharacter.cpp
+++ Source/ProjectMoveItUnreal/Private/Characters/Player/PlayerCharacter.cpp
@@ -3,32 +3,105 @@
 
 #include "Characters/Player/PlayerCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	SpringArm->SetupAttachment(RootComponent);
+	Camera->SetupAttachment(SpringArm);
+
+	SpringArm->bInheritPitch = true;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = true;
+
+	SpringArm->TargetArmLength = 0.0f;
+	SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, SpringArmHeight));
 }
 
-// Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	// Add the input mapping context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputContext, 0);
+		}
+	}
 }
 
-// Called every frame
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveForwardInput, ETriggerEvent::Triggered, this, &APlayerCharacter::MoveForward);
+		EnhancedInputComponent->BindAction(MoveRightInput, ETriggerEvent::Triggered, this, &APlayerCharacter::MoveRight);
+		EnhancedInputComponent->BindAction(LookInput, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpInput, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
+		
+	}
+}
+
+void APlayerCharacter::MoveForward(const FInputActionValue& Value)
+{
+	if(Controller != nullptr)
+	{
+		const FRotator Rotator = Controller->GetControlRotation();
+		const FRotator YawRotation(0.0f, Rotator.Yaw, 0.0f);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector2D InputVector = Value.Get<FVector2D>();
+		AddMovementInput(ForwardDirection, InputVector.Y);
+
+		UE_LOG(LogTemp, Warning, TEXT("%f"), InputVector.Y);
+	}
+}
+
+void APlayerCharacter::MoveRight(const FInputActionValue& Value)
+{
+	const FRotator Rotator = Controller->GetControlRotation();
+	const FRotator YawRotation(0.0f, Rotator.Yaw, 0.0f);
+	
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	const FVector2D InputVector = Value.Get<FVector2D>();
+	AddMovementInput(RightDirection, InputVector.X);
+	
+	UE_LOG(LogTemp, Warning, TEXT("%f"), InputVector.X);
+}
+
+void APlayerCharacter::Jump(const FInputActionValue& Value)
+{
+	Super::Jump();
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D InputVector = Value.Get<FVector2D>();
+	if(bInvertedLook)
+	{
+		AddControllerYawInput(InputVector.X);
+		AddControllerPitchInput(InputVector.Y);
+	}
+	else if(!bInvertedLook)
+	{
+		AddControllerYawInput(-InputVector.X);
+		AddControllerPitchInput(InputVector.Y);
+	}
 
 }
 
--- Source/ProjectMoveItUnreal/Public/Characters/Player/PlayerCharacter.h
+++ Source/ProjectMoveItUnreal/Public/Characters/Player/PlayerCharacter.h
@@ -4,26 +4,58 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "PlayerCharacter.generated.h"
+
+class UEnhancedInputComponent;
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class PROJECTMOVEITUNREAL_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
+//functions
 public:
-	// Sets default values for this character's properties
+	//ctor
 	APlayerCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
+	//default functions
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	//custom functions
+	void MoveForward(const FInputActionValue& Value);
+	void MoveRight(const FInputActionValue& Value);
+	void Jump(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+protected:
+	virtual void BeginPlay() override;
+private:
 
+//variables
+public:
+//input assets
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveForwardInput;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveRightInput;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookInput;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpInput;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* InputContext;
+
+//actor components
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* Camera;
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* SpringArm;
+	//actor component variables
+	UPROPERTY(EditAnywhere, Category = "Component Values")
+	float SpringArmHeight = 60.0f;
+	UPROPERTY(EditAnywhere, Category = "Component Values")
+	bool bInvertedLook = false;
+protected:
+private:
 };
